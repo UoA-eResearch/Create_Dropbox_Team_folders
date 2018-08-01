4fd5e73253d64a75cf5e02ee12b82aa4204f664c
robertburrowes <r.burrowes@auckland.ac.nz>
2018-08-02 10:07:43 +1200

    combined https_session with http_session. Fix "get" headers
    Consistent use of @cookies over @cookie in project
    Get wasn't setting header fields correctly.

--- rlib/webbrowser.rb
+++ rlib/webbrowser.rb
@@ -4,7 +4,8 @@ require 'uri'
 require 'nokogiri'
 require 'base64'
 
-#Code borrowed from WIKK WebBrowser class under MIT Lic. https://github.com/wikarekare.
+#Code from WIKK WebBrowser class under MIT Lic. https://github.com/wikarekare.
+
 class WebBrowser
   
   class Error < RuntimeError
@@ -18,19 +19,28 @@ class WebBrowser
 
   attr_reader :host
   attr_accessor :session
-  attr_accessor :cookie
+  attr_accessor :cookies
   attr_reader :page
   attr_accessor :referer
   attr_accessor :debug
+  attr_accessor :verify_cert
+  attr_accessor :port
+  attr_accessor :use_ssl
+  
 
   #Create a WebBrowser instance
   # @param host [String] the host we want to connect to
+  # @param port [Fixnum] Optional http server port
+  # @param use_ssl [Boolean] Use https, if true
+  # @param verify_cert [Boolean] Validate certificate if true (Nb lots of embedded devices have self signed certs, so verify will fail)
   # @return [WebBrowser]
-  def initialize(host:)
+  def initialize(host:, port: nil, use_ssl: false, cookies: nil, verify_cert: true, debug: false)
     @host = host  #Need to do this, as passing nil is different to passing nothing to initialize!
-    @cookies = nil
-    @debug = false
-    @ssl = false
+    @cookies = cookies
+    @debug = debug
+    @use_ssl = use_ssl
+    @port = port != nil ? port : ( use_ssl ? 443 : 80 )
+    @verify_cert = verify_cert
   end
 
   #Create a WebBrowser instance, connect to the host via http, and yield the WebBrowser instance.
@@ -39,9 +49,9 @@ class WebBrowser
   # @param port [Fixnum] (80) the port the remote web server is running on
   # @param block [Proc] 
   # @yieldparam [WebBrowser] the session descriptor for further calls.
-  def self.http_session(host:, port: 80)
-    wb = self.new(host: host)
-    wb.http_session(port: port) do
+  def self.http_session(host:, port: nil, debug: false, cookies: nil)
+    wb = self.new(host: host, port: port, debug: debug, use_ssl: false, cookies: cookies)
+    wb.http_session do
       yield wb
     end
   end
@@ -53,9 +63,9 @@ class WebBrowser
   # @param verify_cert [Boolean] Validate certificate if true (Nb lots of embedded devices have self signed certs, so verify will fail)
   # @param block [Proc] 
   # @yieldparam [WebBrowser] the session descriptor for further calls.
-  def self.https_session(host:, port: 443, verify_cert: true)
-    wb = self.new(host: host)
-    wb.https_session(port: port, verify_cert: verify_cert) do
+  def self.https_session(host:, port: nil, verify_cert: true, cookies: nil, debug: false)
+    wb = self.new(host: host, port: port, cookies: cookies, use_ssl: true, verify_cert: verify_cert, debug: debug)
+    wb.http_session do
       yield wb
     end
   end
@@ -63,26 +73,18 @@ class WebBrowser
   #Creating a session for http connection
   #  attached block would then call get or post NET::HTTP calls
   # @param port [Fixnum] Optional http server port
-  # @param ssl [Boolean] Use https, if true
+  # @param use_ssl [Boolean] Use https, if true
   # @param verify_cert [Boolean] Validate certificate if true (Nb lots of embedded devices have self signed certs, so verify will fail)
   # @param block [Proc] 
-  def http_session(port: 80, ssl: false, verify_cert: true)
-    @http = Net::HTTP.new(@host, port)   
-    @ssl = @http.use_ssl = ssl      
-    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE  if ! verify_cert 
+  def http_session
+    @http = Net::HTTP.new(@host, @port)   
+    @http.set_debug_output($stdout) if @debug
+    @http.use_ssl = @use_ssl      
+    @http.verify_mode = OpenSSL::SSL::VERIFY_NONE  if ! @use_ssl || ! @verify_cert 
     @http.start do |session| #ensure we close the session after the block
       @session = session 
       yield 
     end
-  end
-
-  #Creating a session for https connection
-  #  attached block would then call get or post NET::HTTP calls
-  # @param port [Fixnum] Optional http server port
-  # @param verify_cert [Boolean] Validate certificate if true (Nb lots of embedded devices have self signed certs, so verify will fail)
-  # @param block [Proc] 
-  def https_session(port: 443, verify_cert: true)
-    http_session(port: port, ssl: true, verify_cert: verify_cert) { yield }
   end
   
   #Web basic authentication (not exactly secure)
@@ -102,7 +104,7 @@ class WebBrowser
   end
 
   #send the query to the web server using an http get, and returns the response.
-  #  Cookies in the response get preserved in @cookie, so they will be sent along with subsequent calls
+  #  Cookies in the response get preserved in @cookies, so they will be sent along with subsequent calls
   #  We are currently ignoring redirects from the PDU's we are querying.
   # @param query [String] The URL after the http://host/ bit and not usually not including parameters, if form_values are passed in
   # @param form_values [Hash{String=>Object-with-to_s}] The parameter passed to the web server eg. ?key1=value1&key2=value2...
@@ -111,16 +113,20 @@ class WebBrowser
   def get_page(query: ,form_values: nil, authorization: nil)
     $stderr.puts "Debugging On" if @debug
     query += form_values_to_s(form_values, query.index('?') != nil) #Should be using req.set_form_data, but it seems to by stripping the leading / and then the query fails.
-    url = URI.parse("#{@ssl ? "https" : "http"}://#{@host}/#{query}")
+    url = URI.parse("#{@use_ssl ? "https" : "http"}://#{@host}/#{query}")
     $stderr.puts url if @debug
-    req = Net::HTTP::Get.new(url.path)  
-    header['Authorization'] = authorization if authorization != nil
-    req['User-Agent'] = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'
-    req['Host']  = "#{@host}"
-    req['Cookie'] = @cookie if @cookie != nil
+    
+    req = Net::HTTP::Get.new(url.request_uri)  
 
-   # req.initialize_http_header( header )
-   # $stderr.puts req if @debug
+    header = { 'HOST' => @host }
+    header['Connection'] = 'keep-alive'
+    header['Accept-Encoding'] = 'gzip, deflate'
+    header['Accept'] = '*/*'
+    header['User-Agent'] = 'curl/7.54.0'
+    #header['Content-Type'] = content_type   
+    header['Cookie'] = @cookies if @cookies != nil
+    header['Authorization'] = authorization if authorization != nil
+    req.initialize_http_header( header )
 
     response = @session.request(req)      
     $stderr.puts response.code.to_i if @debug
@@ -132,9 +138,9 @@ class WebBrowser
           #response.each {|key, val| $stderr.printf "%s = %s\n", key, val }  #Location seems to have cgi params removed. End up with .../cginame?&
           #$stderr.puts "Redirect to #{response['location']}"   #Location seems to have cgi params removed. End up with .../cginame?&
           if (response_text = response.response['set-cookie']) != nil
-            @cookie =  response_text
+            @cookies =  response_text
           else
-            @cookie = ''
+            @cookies = ''
           end
           #$stderr.puts
         return
@@ -145,9 +151,9 @@ class WebBrowser
     end
 
     if (response_text = response.response['set-cookie']) != nil
-      @cookie =  response_text
+      @cookies =  response_text
     else
-      @cookie = ''
+      @cookies = ''
     end
 
     return response.body
@@ -163,13 +169,13 @@ class WebBrowser
   def post_page(query:, authorization: nil, content_type: 'application/x-www-form-urlencoded', data: nil)
     #query += form_values_to_s(form_values) #Should be using req.set_form_data, but it seems to by stripping the leading / and then the query fails.
    #puts query
-    url = URI.parse("#{@ssl ? "https" : "http"}://#{@host}/#{query}")
+    url = URI.parse("#{@use_ssl ? "https" : "http"}://#{@host}/#{query}")
     req = Net::HTTP::Post.new(url.path)
     header = {'HOST' => @host}
     header['User-Agent'] = 'curl/7.54.0'
     header['Authorization'] = authorization if authorization != nil
     header['Content-Type'] = content_type   
-    header['Cookie'] = @cookie if @cookie != nil
+    header['Cookie'] = @cookies if @cookies != nil
     req.initialize_http_header( header )
     #puts req.methods
     #req.set_form_data(form_values, '&') if form_values != nil
@@ -183,9 +189,9 @@ class WebBrowser
             #response.each {|key, val| printf "%s = %s\n", key, val }  #Location seems to have cgi params removed. End up with .../cginame?&
             #puts "Redirect of Post to #{response['location']}" #Location seems to have cgi params removed. End up with .../cginame?&
             if (response_text = response.response['set-cookie']) != nil
-              @cookie =  response_text
+              @cookies =  response_text
             else
-              @cookie = ''
+              @cookies = ''
             end
           return
         end
@@ -193,9 +199,9 @@ class WebBrowser
       end
 
       if (response_text = response.response['set-cookie']) != nil
-        @cookie =  response_text
+        @cookies =  response_text
       else
-        @cookie = ''
+        @cookies = ''
       end
 
       @response = response
@@ -244,3 +250,4 @@ class WebBrowser
     return s
   end
 end
+
