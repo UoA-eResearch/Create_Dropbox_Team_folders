e6674fbf1b6d0ca752b9ea169bf1708c92e0d64a
robertburrowes <r.burrowes@auckland.ac.nz>
2018-08-19 00:59:11 +1200

    User commands now run under team_file_token
    Also catch and ignore WebBrowser errors

--- bin/add_ldap_group_to_dropbox.rb
+++ bin/add_ldap_group_to_dropbox.rb
@@ -4,8 +4,8 @@ require_relative '../rlib/ldap.rb' #json to class with accessor methods
 require_relative '../rlib/dropbox.rb' #json to class with accessor methods
 require_relative '../rlib/uoa.rb' #Localized front ends to dropbox and ldap calls.
 
-DRYRUN=true #Run through actions, printing what would have been done, but don't execute them
-TRACE=true #Dump output of calls to dropbox
+DRYRUN=false #Run through actions, printing what would have been done, but don't execute them
+TRACE=false #Dump output of calls to dropbox
 
 conf_file = "#{File.expand_path(File.dirname(__FILE__))}/../conf/auth.json"
 @conf = WIKK::Configuration.new(conf_file)
@@ -13,12 +13,17 @@ conf_file = "#{File.expand_path(File.dirname(__FILE__))}/../conf/auth.json"
 @dbx_file = Dropbox.new(token: @conf.team_file_token)
 @dbx_mng = Dropbox.new(token: @conf.team_management_token)
 @dbx_info = Dropbox.new(token: @conf.team_info_token)
-@dbx_person = Dropbox.new(token: @conf.user_token)
+#@dbx_person = Dropbox.new(token: @conf.user_token, as_admin: true)
+@dbx_person = Dropbox.new(token: @conf.team_file_token, as_admin: true)
 
 research_projects = JSON.parse(File.read("#{File.expand_path(File.dirname(__FILE__))}/../conf/projects.json"))
 
 research_projects.each do |rp|
-  research_project = {:research_code => rp["research_code"], :team_folder => rp["team_folder"]}
-  create_dropbox_team_folder_from_research_code(research_projects: research_project, dryrun: DRYRUN, trace: TRACE)
-  puts
+  begin
+    research_project = {:research_code => rp["research_code"], :team_folder => rp["team_folder"]}
+    create_dropbox_team_folder_from_research_code(research_projects: research_project, dryrun: DRYRUN, trace: TRACE)
+    puts
+  rescue WebBrowser::Error => e
+    #Ignore these and try the next group.
+  end
 end
