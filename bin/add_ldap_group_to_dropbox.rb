88b7e45ebbf99082a8e7e39ed10715fcce4a7a0b
robertburrowes <r.burrowes@auckland.ac.nz>
2021-02-19 13:59:34 +1300

    Code to process conf/exceptions.json
    exceptions.json records manally maintained users.
    
    It provides a record of Admin accounts
    
    It allows adding of users that aren't in any research project
    
    It allows overriding of the AD email field, if the IDP is responding with a different email address (e.g. happen when staff leave, or go back to being students).

--- bin/add_ldap_group_to_dropbox.rb
+++ bin/add_ldap_group_to_dropbox.rb
@@ -1,58 +1,63 @@
 #!/usr/local/bin/ruby
+require 'time'
 require 'wikk_configuration'
 require_relative '../rlib/ldap.rb' #json to class with accessor methods
 require_relative '../rlib/dropbox.rb' #json to class with accessor methods
 require_relative '../rlib/uoa.rb' #Localized front ends to dropbox and ldap calls.
 
-DRYRUN=false #Run through actions, printing what would have been done, but don't execute them
-TRACE=false #Dump output of calls to dropbox
+DRYRUN=false # Run through actions, printing what would have been done, but don't execute them
+TRACE=false # Dump output of calls to dropbox
 
-#Read configuration file and initialize connections
+# Read configuration file and initialize connections
 def init_connections
-  conf_file = "#{File.expand_path(File.dirname(__FILE__))}/../conf/auth.json"
+  conf_file = "#{__dir__}/../conf/auth.json"
   @conf = WIKK::Configuration.new(conf_file)
 
   @ldap = UOA_LDAP.new(conf: @conf)
 
-  #Team member file access – Team information and auditing, plus the ability to perform any action as any team member
+  # Team member file access – Team information and auditing, plus the ability to perform any action as any team member
   @dbx_file = Dropbox.new(token: @conf.team_file_token)
 
-  #Team member management – Team information, plus the ability to add, edit, and delete team members
+  # Team member management – Team information, plus the ability to add, edit, and delete team members
   @dbx_mng = Dropbox.new(token: @conf.team_management_token)
 
-  #Team information – Information about the team and aggregate usage data
+  # Team information – Information about the team and aggregate usage data
   @dbx_info = Dropbox.new(token: @conf.team_info_token) 
 
-  #Team member file access – Team information and auditing, plus the ability to perform any action as any team member
-  #In this case, impersonating an admin user to perform user based calls. 
-  #Replaces using an Admin's user_token, which no longer works. 
-  #@dbx_person = Dropbox.new(token: @conf.user_token, as_admin: true)
+  # Team member file access – Team information and auditing, plus the ability to perform any action as any team member
+  # In this case, impersonating an admin user to perform user based calls. 
+  # Replaces using an Admin's user_token, which no longer works. 
+  # @dbx_person = Dropbox.new(token: @conf.user_token, as_admin: true)
   @dbx_person = Dropbox.new(token: @conf.team_file_token, admin_id: @conf.admin_id)
 end
 
-#Do housekeeping, by ensuring all manually added people have their profile set correctly.
-def update_existing_team_members_email_addresses
+# Do housekeeping, by ensuring all manually added people have their profile set correctly.
+# Don't override the exceptions file, but do override any entries not in this file.
+def update_existing_team_members_profile
   cache_all_team_members(trace: TRACE)
+  
+  # These entries don't have all their fields set, so were manually entered.
+  # Adding an @aucklanduni user manually, when they already exist with their staff email, will log an exception.
   @partial_entries.each do |v|
     puts "Notice: Manually updating user profile for #{v["email"]} from UoA LDAP"
     begin
       update_team_users_profiles(email: v["email"])
     rescue StandardError => e
-      puts "Error: #{e}"
+      warn "Error: #{e}"
     end
   end
 
-  #Reload the team members, if we modified any of them.
+  # Reload the team members, if we modified any of them.
   cache_all_team_members(trace: TRACE) if @partial_entries.length != 0
 end
 
-#Reads list of research groups using dropbox (currently from a json file, but later will be from research project DB)
-#Adds new team members to dropbox
-#Creates and populates or updates existing dropbox rw and ro groups for each research project
-#Creates team folders, if they are missing
-#Sets team folder ACLs using rw and ro groups just created
+# Reads list of research groups using dropbox (currently from a json file, but later will be from research project DB)
+# Adds new team members to dropbox
+# Creates and populates or updates existing dropbox rw and ro groups for each research project
+# Creates team folders, if they are missing
+# Sets team folder ACLs using rw and ro groups just created
 def process_each_research_project_using_dropbox
-  research_projects = JSON.parse(File.read("#{File.expand_path(File.dirname(__FILE__))}/../conf/projects.json"))
+  research_projects = JSON.parse(File.read("#{__dir__}/../conf/projects.json"))
 
   research_projects.each do |rp|
     begin
@@ -60,11 +65,89 @@ def process_each_research_project_using_dropbox
       create_dropbox_team_folder_from_research_code(research_projects: research_project, dryrun: DRYRUN, trace: TRACE)
       puts
     rescue WebBrowser::Error => e
-      #Ignore these and try the next group.
+      # Ignore these and try the next group.
+    end
+  end
+end
+
+def existing_research_group?(groupname:)
+  return false
+end
+
+def process_manual_groups(dryrun: DRYRUN, trace: TRACE)
+  return if @manual_users.length == 0
+  
+  # Add in users from the exceptions file, if they don't already exist in dropbox.
+  failed_to_add = add_missing_members(members_arr: @manual_users.values, dryrun: dryrun, trace: trace)
+  
+  # Add users to the dropbox groups, as per exceptions.json 
+  @manual_groups.each do |groupname, members|
+    next if existing_research_group?(groupname: groupname) # No manual overriding of LDAP research groups.
+    begin
+      email_address_list = []  # create a blank email list for this group
+      members.each { |m| email_address_list << m.email } # add users emails to the group
+      failed_to_add.each { |email| email_address_list.delete(email) }
+      # Do a diff, and add or remove users from the dropbox group.
+      update_dropbox_group(group_name: groupname, email_list: email_address_list, dryrun: dryrun, trace: trace)
+    rescue WebBrowser::Error => e
+    end
+  end
+  
+  @manual_users.each do |upi, ldap_entry|
+    if @team_member_map[upi].nil?
+      warn "process_manual_groups: Unknown dropbox account, when changing role, for #{upi}"
+    elsif @team_member_map[upi]['role'] != ldap_entry.role
+      if dryrun
+        puts "Changing role of #{upi} to #{ldap_entry.role}"
+      else
+        @dbx_mng.team_members_set_admin_permissions(team_member_id: @team_member_map[upi]['team_member_id'], role: ldap_entry.role, trace: trace)
+      end
+    elsif dryrun
+      puts "Role of #{upi} unchanged from #{ldap_entry.role}"
+    end
+  end
+end
+
+# Reads conf/exceptions.json and processes the entries
+# This file allows
+# * manually adding of users, that would not be in any research project group
+# * manual assignment of users to groups (non-research groups)
+# * overriding the email address (Necessary, where there is an IDP email field mismatch with the AD mail field)
+def init_exceptions
+  @manual_users = {}     # Users we in the exceptions.json file (still required to have a UoA account to use the UoA IDP)
+  @manual_groups = {}    # Groups we are creating in dropbox. These shouldn't be a research projects group in the AD
+  @manual_groups['user_added_manually'] = [] # Create empty array for this default group, we add all exceptions too. This has no team folder.
+  
+  # e.g.   "rbur004": { "email": "", "role": "Team admin", "group": ["UoA Admins"], "note": "CeR Rob Burrowes", "expires": "9999-12-31"},
+  @manual_entries = JSON.parse(File.read("#{__dir__}/../conf/exceptions.json"))
+  @manual_entries.each do |upi, r|
+    next if upi == 'comment'  #Skip the comment at the start.
+    if Time.parse(r['expires']) > Time.now # Entry is still valid
+      manual_email = (r['email'].nil? || r['email'] == '') ? nil : r['email'].downcase 
+      new_user_entry = @ldap.get_ldap_user_attributies(upi: upi, attributes: {'sn'=>'surname', 'givenname'=>'given_name', 'mail'=>'email', 'cn'=>'external_id'})
+      new_user_entry.role = r['role']
+      
+      # Manually added users still need to be in the UoA AD to be in the UoA dropbox team.
+      unless new_user_entry.nil?
+        new_user_entry.email = manual_email unless manual_email.nil? # Replace LDAP email address, with our version.
+        @manual_users[upi] = new_user_entry
+        
+        @manual_groups['user_added_manually'] << new_user_entry
+        if r['group'].class == Array
+          r['group'].each do |g|
+            @manual_groups[g] ||= [] # Create array, if it didn't already exist for this group
+            @manual_groups[g] << new_user_entry
+          end
+        end
+      else
+        warn "exception.json: #{upi} Need to have a UoA identity (user not found in AD)" 
+      end
     end
   end
 end
 
 init_connections
-update_existing_team_members_email_addresses
+init_exceptions
+update_existing_team_members_profile
 process_each_research_project_using_dropbox
+process_manual_groups
